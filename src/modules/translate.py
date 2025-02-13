6311ec22add3363e8bc4cf3ed96e18d452e4ae58
gabepen <gabrielpenun@gmail.com>
2025-02-13 15:07:01 -0800

    added function for determining non-synonomous mutaiton potential in codon table
    will be used to properly normalize dn/ds rates

--- src/modules/translate.py
+++ src/modules/translate.py
@@ -9,29 +9,92 @@ import numpy as np
 from Bio import SeqIO
 from BCBio import GFF
 from modules.parse import SeqContext
+from typing import Dict, List, Tuple, Optional
 
-def table_lookup(codon):
-    '''
-    for use with table in dict format 
+def table_lookup(codon: str) -> str:
+    '''Look up amino acid for a given codon using global codon table.
+    
+    Args:
+        codon (str): Three letter nucleotide codon
+        
+    Returns:
+        str: Single letter amino acid code, or '@' if codon not found
     '''
     for aa in table.keys():
         if codon in table[aa] and aa != 'starts':
             return aa
     return '@'
 
-def hgvs_notation(mut):
+def ems_immune_codons(codon_table: Dict[str, List[str]]) -> List[str]:
+    '''Find codons that only produce synonymous mutations from EMS changes.
+    
+    Args:
+        codon_table: Dictionary mapping amino acids to their codons
+        
+    Returns:
+        List[str]: List of codons that can only produce synonymous mutations 
+                  from C>T or G>A changes
     '''
-    Converts mutations from mut_jsons to hgvs notation
+    # Create reverse lookup (codon -> amino acid)
+    codon_to_aa = {}
+    for aa, codons in codon_table.items():
+        if aa != 'starts':  # Skip start codons list
+            for codon in codons:
+                codon_to_aa[codon] = aa
+    
+    immune_codons = []
+    
+    for codon in codon_to_aa:
+        original_aa = codon_to_aa[codon]
+        is_immune = True
+        has_ems_site = False
+        
+        # Check each position in the codon
+        for i, base in enumerate(codon):
+            if base == 'C':
+                has_ems_site = True
+                # Test C>T mutation
+                mutated = codon[:i] + 'T' + codon[i+1:]
+                if mutated in codon_to_aa and codon_to_aa[mutated] != original_aa:
+                    is_immune = False
+                    break
+            elif base == 'G':
+                has_ems_site = True
+                # Test G>A mutation
+                mutated = codon[:i] + 'A' + codon[i+1:]
+                if mutated in codon_to_aa and codon_to_aa[mutated] != original_aa:
+                    is_immune = False
+                    break
+        
+        if is_immune and has_ems_site:
+            immune_codons.append(codon)
+            
+    return sorted(immune_codons)
+
+
+def hgvs_notation(mut: str) -> str:
+    '''Convert mutation from internal format to HGVS notation.
+    
+    Args:
+        mut (str): Mutation in format "position_ref>alt"
+        
+    Returns:
+        str: Mutation in HGVS format "refPOSalt"
     '''
     pos = int(mut.split('_')[0]) + 1
     residues = mut.split('_')[1]
     ref = residues.split('>')[0]
     alt = residues.split('>')[1]
-    return ref+str(pos)+alt
+    return ref + str(pos) + alt
     
-def single_flip(base):
-    '''
-    Flips to paired base
+def single_flip(base: str) -> str:
+    '''Get complementary base pair for a nucleotide.
+    
+    Args:
+        base (str): Single nucleotide (A, T, C, or G)
+        
+    Returns:
+        str: Complementary nucleotide
     '''
     if base == 'A':
         return 'T'
@@ -42,11 +105,20 @@ def single_flip(base):
     elif base == 'G':
         return 'C'
 
-def translate(seq, fasta_bool):
-    '''
-    Given nucleic acid sequence, converts to coresponding AA seq
+def translate(seq: str, fasta_bool: bool) -> Tuple[Optional[str], int]:
+    '''Convert nucleic acid sequence to amino acid sequence.
+    
+    Args:
+        seq (str): Nucleotide sequence
+        fasta_bool (bool): Whether to format output as FASTA with line breaks
+        
+    Returns:
+        Tuple containing:
+            - Optional[str]: Amino acid sequence, or None if no start codon found
+            - int: Length of amino acid sequence, or 1 if translation failed
     '''
     aa_seq = ''
+    start = -1
     for i in range(len(seq)):
         if seq[i:i+3] in table['starts']:
             start = i
@@ -54,21 +126,22 @@ def translate(seq, fasta_bool):
     if start == -1:
         print(seq)
         return None, 1
-    #split seq into codons
+        
+    # Split seq into codons
     codons = []
     for pos1, pos2, pos3 in zip(seq[start::3], seq[start+1::3], seq[start+2::3]):
        codons.append(''.join([pos1, pos2, pos3]))
-    #line count
+       
+    # Convert codons to amino acids
     line_count = 0
     length = 0
     for codon in codons:
         line_count += 1
         length += 1
         aa_seq += table_lookup(codon)
-        if fasta_bool:
-            if line_count >= 50:
-                aa_seq += '\n'
-                line_count = 0
+        if fasta_bool and line_count >= 50:
+            aa_seq += '\n'
+            line_count = 0
 
     return aa_seq, length
 
